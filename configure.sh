c18e695b678b02a2d7d4159c0b41d265c3b57fa8
xsoloking <soloking.x@gmail.com>
2023-10-08 14:40:54 +0800

    Update default ip address

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 # Modify default system settings
 
 # 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate 
 
 # Hello World
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default

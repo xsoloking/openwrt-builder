c397b1be058398e261b79e01fa50dffd11fee8c4
xsoloking <soloking.x@gmail.com>
2026-04-26 21:29:15 +0800

    Update package.sh to manage xray-core and helloworld
    Removed xray-core from feeds and added helloworld package.

--- package.sh
+++ package.sh
@@ -10,3 +10,8 @@ git clone --depth 1 -b master  https://github.com/UnblockNeteaseMusic/luci-app-u
 
 # OpenClash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+
+rm -rf package/feeds/packages/xray-core
+git clone --depth 1 https://github.com/fw876/helloworld package/helloworld
+cp -r package/helloworld/xray-core package/xray-core
+rm -rf package/helloworld
